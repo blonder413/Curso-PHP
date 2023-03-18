0a835998ef392513ff76a7743f6a6ce7ee43e69a
Jonathan Morales <blonder413@gmail.com>
2023-03-18 15:18:51 -0500

    metodos magicos

--- sobrecarga.php
+++ sobrecarga.php
@@ -4,6 +4,22 @@
 
 class Cliente {
 
+    private $nombre = 'jonathan';
+    public $nick = 'blonder413';
+
+    /**
+     * Este método es invocado por var_dump() al volcar un objeto para obtener las propiedades que deberían mostrarse. 
+     * Si el método no está definido se mostrarán todas las propiedades públicas, protegidas y privadas.
+     */
+    public function __debugInfo() {
+        return [
+            'nick' => $this->nick,
+        ];
+    }
+    
+    /**
+     * Es llamado cuando se intenta acceder a un atributo que no existe
+     */
     function __get($atributo)
     {
         //return $this->$atributo;
@@ -14,10 +30,28 @@ class Cliente {
     {
         $this->$atributo = $valor;
     }
+    
+    /**
+     * Es llamado cuando se imprime la clase directamente
+     */
+    public function __toString()
+    {
+        return 'hola, soy una clase';
+    }
+    
+    /**
+     * Es llamado cuando se intenta llamar una instancia como si fuera un método
+     */
+    public function __invoke($x)
+    {
+        return('esto no es un método, es un objeto');
+    }
 
 }
 
 $cliente = new Cliente();
 //$cliente->nombre = "blonder";
-
+echo $cliente(5);
 echo $cliente->blog;
+echo $cliente;
+var_dump($cliente);
