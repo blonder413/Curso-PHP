79a7687e40fe74d8ebbe330fdd14f8c95f5bd11b
Jonathan Morales <blonder413@gmail.com>
2023-11-12 10:05:12 -0500

    funciones que reciben varios parámetros

--- funciones/funciones2.php
+++ funciones/funciones2.php
@@ -29,6 +29,23 @@ saludar('blonder');
 
 
 /**
+ * Cuando una función define varios parámetros algunos de ellos pueden ser opcionales
+ * Para definir parámetros opcionales se deben establecer de derecha a izquierda,
+ * es decir, los oblitarorios deben estar en primer orden
+ */
+function sumar($valor1, $valor2 = 0)
+{
+    return $valor1 + $valor2;
+}
+
+/**
+ * Al llamar al método sumar, el valor2 es opcional
+ * no es posible definir la función con el valor1 inicial y el 2 obligatorios
+ */
+sumar(1);
+sumar(4,13)
+
+/**
  * Las variables definidas en las funciones son locales,
  * es decir, no afectan variables fuera de ellas
  * Para nombrar una función puede usarse lower camel case o snake case
