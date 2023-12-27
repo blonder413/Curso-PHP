fc8a9a370d42b8ff6b9b15b3008e5a7b4c73aa14
Jonathan Morales <blonder413@gmail.com>
2023-12-27 09:34:41 -0500

    operador ternario

--- condicionales/if_5.php
+++ condicionales/if_5.php
@@ -13,6 +13,7 @@ if (is_null($a)) {
 }
 
 /**
+ * Ternary Operator
  * El condicional anterior se puede resumir en una línea
  * condición ? valor si se cumple la condición : valor si no se cumple la condición
  */
@@ -20,6 +21,15 @@ $c = is_null($a) ? 0 : $a;
 echo $c;
 
 /**
+ * El 0 lo toma como falso por lo tanto evalúa la siguiente expresión
+ * así continúa hasta que encuentre un valor válido
+ */
+echo 0 ?: 1 ?: 2 ?: 3, PHP_EOL; //1
+echo 0 ?: 0 ?: 2 ?: 3, PHP_EOL; //2
+echo 0 ?: 0 ?: 0 ?: 3, PHP_EOL; //3
+
+/**
+ * Null Coalescing Operator
  * El operador ?? valida si el primero valor es null
  * si es falso asigna el primer valor, si no asigna el siguiente
  * Es un operador de comparación muy útil para validar si un valor es null
